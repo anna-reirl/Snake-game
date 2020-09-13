cab2137c2ab35be15afe0a8f94a749168caa6bed
Anna <anna_agabekyan_anna@mail.ru>
2020-09-13 16:49:38 +0400

    feat: draw objects, as well as add functionality to the game

--- js/game.js
+++ js/game.js
@@ -22,6 +22,20 @@ snake[0] = {
     y: 10 * box
 };
 
+document.addEventListener('keydown', direction);
+
+let dir;
+
+function direction(event) {
+    if(event.keyCode == 37 && dir != 'right')
+    dir = 'left';
+    else if(event.keyCode == 38 && dir != 'down')
+    dir = 'up';
+    else if(event.keyCode == 39 && dir != 'left')
+    dir = 'right';
+    else if(event.keyCode == 40 && dir != 'up')
+    dir = 'down';
+}
 
 function drawGame() {
     ctx.drawImage(ground, 0, 0);
@@ -29,13 +43,39 @@ function drawGame() {
     ctx.drawImage(foodImg, food.x, food.y)
 
     for (let i = 0; i < snake.length; i++) {
-        ctx.fillStyle = "green";
+        ctx.fillStyle = i == 0 ? "green" : "red";
         ctx.fillRect(snake[i].x, snake[i].y, box, box);
     }
 
     ctx.fillStyle = "white";
     ctx.font = "50px Arial";
     ctx.fillText(score, box * 2.5, box * 1.7)
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(snakeX == food.x && snakeY == food.y) {
+        score++;
+        food = {
+            x: Math.floor((Math.random() * 17 + 1)) * box,
+            y: Math.floor((Math.random() * 15 + 3)) * box
+        };
+    } else {
+        snake.pop();
+    }
+
+
+    if(dir == "left") snakeX -= box;
+    if(dir == "right") snakeX += box;
+    if(dir == "up") snakeY -= box;
+    if(dir == "down") snakeY += box;
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    };
+
+    snake.unshift(newHead);
 }
 
 let game = setInterval(drawGame, 100);
